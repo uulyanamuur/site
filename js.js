e747b7decaf5b836055c560bdcb2c8fb44020b79
uulyanamuur <57221333+uulyanamuur@users.noreply.github.com>
2020-12-01 02:02:48 +0300

    new site

---
+++ js.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	$('.headrburger').click(function(event){
+		$('.headrburger,.headrmenu, .contents').toggleClass('active');
+		$('body').toggleClass('lock');
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	});
+});
+
+
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
+var rootElement = document.documentElement
+
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+  if ((rootElement.scrollTop / scrollTotal ) > 0.80) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn")
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn")
+  }
+}
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
+document.addEventListener("scroll", handleScroll)
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
